67a6fff013c7a45c7999fcc8bb3bf2c43bede116
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-16 16:26:32 +0300

    added content to the child component that displays user's profiles. Then linked the template of the child component to the parent component

--- src/app/samsoluoch/samsoluoch.component.ts
+++ src/app/samsoluoch/samsoluoch.component.ts
@@ -12,6 +12,9 @@ export class SamsoluochComponent implements OnInit {
   Samsoluochs = [
     new Samsoluoch (1, 'Sam Oluoch', 'Anitas Kitchen'),
     new Samsoluoch (2, 'Sam Oluoch', 'Quotes'),
+    new Samsoluoch (3, 'Sam Oluoch', 'Ping-Pong'),
+    new Samsoluoch (4, 'Sam Oluoch', 'Pig Dice'),
+    new Samsoluoch (5, 'Sam Oluoch', 'Rating Restaurant'),
   ];
 
   constructor() { }
