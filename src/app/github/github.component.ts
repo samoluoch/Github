69a47f680376296421b093d44287886a65ef9842
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-16 15:26:15 +0300

    imported class Github and class Samsoluoch into the main component. Also created an array of Samsoluoch profile and repository names

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Samsoluoch} from '../github';
+import {Github} from '../github';
 
 @Component({
   selector: 'app-github',
@@ -6,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
+  Samsoluoch = [
+    new Samsoluoch (1, 'Sam Oluoch', 'Anitas Kitchen'),
+    new Samsoluoch (2, 'Sam', 'Quotes'),
+  ];
 
-  constructor() { }
+  // constructor() { }
 
   ngOnInit() {
   }
