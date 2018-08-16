b1b82155c94fb5dc8df45333616b0e9582ae63d3
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-16 17:12:12 +0300

    created object instances in the users class and imported and exported the users class to the users component. Created an array for users that holds usernames and id only

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Users} from '../users';
 
 @Component({
   selector: 'app-users',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+
+  User = [
+    new Users (1, 'samsoluoch'),
+    new Users (2, 'samsoluoch'),
+    new Users (3, 'samsoluoch'),
+    new Users (4, 'samsoluoch'),
+    new Users (5, 'samsoluoch'),
+  ];
 
   constructor() { }
 
