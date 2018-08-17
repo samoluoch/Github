f239c38118b23641b446b7bfc136d81c269a8a3a
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-17 11:50:52 +0300

    registered the GithubService in the parent component as a provider

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Repositories} from '../repository';
+import { GithubService } from '../repository/github.service';
 
 @Component({
   selector: 'app-github',
   templateUrl: './github.component.html',
+  providers: [GithubService],
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
