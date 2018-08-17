d2ed6e9e81809129fedef6fa61de6519ceefa830
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-17 22:51:29 +0300

    fetched more details using the API to the samsoluoch profile

--- src/app/samsoluoch/samsoluoch.component.ts
+++ src/app/samsoluoch/samsoluoch.component.ts
@@ -18,11 +18,13 @@ export class SamsoluochComponent implements OnInit {
     interface ApiResponse {
       avatar_url;
       login;
+      public_repos;
+      repos_url;
     }
     this.http.get<ApiResponse>("https://api.github.com/users/samsoluoch?access_token=698c4dedb5ca335cf05a7b84facdf3e811e45237").subscribe(information =>{
     console.log(information);
-        this.samsoluoch = new Users(information.avatar_url, information.login);
-  }
+        this.samsoluoch = new Users(information.avatar_url, information.login, information.public_repos, information.repos_url);
+  });
 
 }
 }
--- src/app/users.ts
+++ src/app/users.ts
@@ -1,4 +1,4 @@
 export class Users {
-  constructor (public profile: any, public login: string) {
+  constructor (public profile: any, public login: string, public public_repos: number, public repos_url: string) {
   }
 }
